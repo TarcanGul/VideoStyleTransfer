9259aa6591c997929e0fd30a04f8891be19ead8b
nailt <nailtarcan@gmail.com>
2019-11-14 14:34:52 -0500

    initial

---
+++ main.py
@@ -0,0 +1,35 @@
+import os
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+import tensorflow.keras.backend as K
+import random
+from scipy.misc import imsave, imresize
+from scipy.optimize import fmin_l_bfgs_b   # https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.fmin_l_bfgs_b.html
+from tensorflow.keras.applications import vgg19
+from tensorflow.keras.preprocessing.image import load_img, img_to_array
+import warnings
+
+def preprocessData(data):
+    pass
+
+def generateImage(input_word):
+    pass
+
+'''
+Main
+'''
+
+def main():
+    print("Starting gif generator.")
+    raw = getRawData()
+    cData = preprocessData(raw[0])   # Content image.
+    sData = preprocessData(raw[1])   # Style image.
+    tData = preprocessData(raw[2])   # Transfer image.
+    styleTransfer(cData, sData, tData)
+    print("Done. Goodbye.")
+
+
+
+if __name__ == "__main__":
+    main()
